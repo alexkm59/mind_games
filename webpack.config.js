58a9d7cdc696fbede86fa74384bcd7223c5eefa8
alexkm59 <alexkm59@yandex.ru>
2023-08-31 20:20:06 +0500

    ошибка с webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: "./main.js",
+    mode: "development",
+    module: {
+        rules: [
+            { test: /\.scss$/, use:["style-loader", "stylelint-scss"] },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+              },
+              {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+              },  
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true
+      },
+      plugins: [new HtmlWebpackPlugin({template: "./index.html",})]
+}
