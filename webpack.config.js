6469572fcdc0e7940b6099f45c97480f95409fbb
alexkm59 <alexkm59@yandex.ru>
2023-09-07 09:36:49 +0500

    дополние конфигурации webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const isProduction = process.env.NODE_ENV === "production"
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 
 module.exports = {
     entry: "./main.js",
-    mode: "development",
+    mode: isProduction ? "production" : "development",
     module: {
         rules: [
-            { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: "asset/resource",
@@ -18,9 +23,13 @@ module.exports = {
             },
         ],
     },
+    optimization: {
+        minimizer: ["...", new CssMinimizerPlugin()],
+    },
+
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: 'bundle.js',
+        filename: "bundle.js",
         clean: true,
     },
     plugins: [
