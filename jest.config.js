c80abc7c92b2fbd8ba51a3dbfc67126eb52341e4
alexkm59 <alexkm59@yandex.ru>
2023-09-17 10:13:07 +0500

    add babel

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'node',
+// };
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+    testEnvironment: "jest-environment-node",
+    setupFilesAfterEnv: ["<rootDir>/jest.config.js"],
+    transform: {
+        "\\.[jt]sx?$": "babel-jest",
+    },
+}
