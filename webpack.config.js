ae3d30f2239e207eced4eac78c721e18d31b4529
alexkm59 <alexkm59@yandex.ru>
2023-09-20 16:19:15 +0500

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
-    entry: "./main.js",
+    entry: "./main.ts",
     mode: "development",
     module: {
         rules: [
