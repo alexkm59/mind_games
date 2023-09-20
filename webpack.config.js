f80503e354c5c5a5b229ec9ede4c5e289618a9f7
alexkm59 <alexkm59@yandex.ru>
2023-09-20 16:21:10 +0500

    webpack config to hw_3_44

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const isProduction = process.env.NODE_ENV === "production"
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 
 module.exports = {
     entry: "./main.ts",
-    mode: "development",
+    mode: isProduction ? "production" : "development",
     module: {
         rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+              },
+
             { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -18,9 +26,17 @@ module.exports = {
             },
         ],
     },
+    optimization: {
+        minimizer: ["...", new CssMinimizerPlugin()],
+    },
+
+
+    resolve: {
+        extensions: [".ts", ".js"],
+      },
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
+        filename: 'bundle.js',
         clean: true,
     },
     plugins: [
