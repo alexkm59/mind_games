c4e0793708e0e06838c19327b0526a24772598da
alexkm59 <alexkm59@yandex.ru>
2023-09-21 15:51:04 +0500

    ошибки с any

--- first_game_field.ts
+++ first_game_field.ts
@@ -1,10 +1,10 @@
 import { timerFunction } from "./timer_function"
-import {cardRandomFunction} from "./cardRandomFunction"
+import { cardRandomFunction } from "./cardRandomFunction"
 //Отрисовываем заголовок игрового поля
 export const renderHeder = () => {
-    const hederElement = document.querySelector(".start-game-field")
-    if (hederElement) {
-        hederElement.innerHTML = `
+  const hederElement = document.querySelector(".start-game-field")
+  if (hederElement) {
+    hederElement.innerHTML = `
         <div class="modal-window"></div>
         <div class="game-field">
                 <div class="heder">
@@ -19,164 +19,159 @@ export const renderHeder = () => {
                 </div>
                 <div class="card-forView"></div> 
                 </div>`
-        hederElement.classList.remove("start-game-field")
-    }
+    hederElement.classList.remove("start-game-field")
+  }
 }
 export const mineGameField = (difficultLevel: string) => {
-    let playCards = 0
-    // const arrCard = ["A", "K", "Q", "J", "10", "9", "8", "7", "6"]
-    const arrSuit = ["Piki", "Cherv", "Bubn", "Krest"]
-    const cardsField = []
+  let playCards = 0
+  // const arrCard = ["A", "K", "Q", "J", "10", "9", "8", "7", "6"]
+  const arrSuit = ["Piki", "Cherv", "Bubn", "Krest"]
+  const cardsField = []
 
-    // let randCard = []
-    let randSuit = []
+  // let randCard = []
+  const randSuit = []
 
-    let totalRandCards = []
-    let totalRandSuits = []
-    let index = []
-    let cardIcon = ""
-    if (difficultLevel === "1") {
-        playCards = 3
-    }
-    if (difficultLevel === "2") {
-        playCards = 6
-    }
-    if (difficultLevel === "3") {
-        playCards = 9
-    }
-    // Создание массивов с случайными рангами и мастями
-    for (let i = 0; i <= playCards - 1; i++) {
-        // randCard[i] = arrCard[Math.floor(Math.random() * arrCard.length)]
-        randSuit[i] = arrSuit[Math.floor(Math.random() * arrSuit.length)]
-    }
-    // totalRandCards = randCard.concat(randCard)
-    totalRandSuits = randSuit.concat(randSuit)
-    // console.log(totalRandCards)
-    console.log(totalRandSuits)
+  let totalRandCards = []
+  let totalRandSuits = []
+  let index = []
+  let cardIcon = ""
+  if (difficultLevel === "1") {
+    playCards = 3
+  }
+  if (difficultLevel === "2") {
+    playCards = 6
+  }
+  if (difficultLevel === "3") {
+    playCards = 9
+  }
+  // Создание массивов с случайными рангами и мастями
+  for (let i = 0; i <= playCards - 1; i++) {
+    // randCard[i] = arrCard[Math.floor(Math.random() * arrCard.length)]
+    randSuit[i] = arrSuit[Math.floor(Math.random() * arrSuit.length)]
+  }
+  // totalRandCards = randCard.concat(randCard)
+  totalRandSuits = randSuit.concat(randSuit)
+  // console.log(totalRandCards)
+  console.log(totalRandSuits)
 
-    totalRandCards = cardRandomFunction(difficultLevel)
+  totalRandCards = cardRandomFunction(difficultLevel)
 
-
-    // Перетасовываем карты
-    for (let i = 0; i < playCards * 2; i++) {
-        index[i] = i
+  // Перетасовываем карты
+  for (let i = 0; i < playCards * 2; i++) {
+    index[i] = i
+  }
+  index = index.sort(() => Math.random() - 0.5)
+  // Вывод карт на просмотр
+  for (let i = 0; i < playCards * 2; i++) {
+    if (totalRandSuits[index[i]] === "Piki") {
+      cardIcon = "iconPiki"
     }
-    index = index.sort(() => Math.random() - 0.5)
-    // Вывод карт на просмотр
-    for (let i = 0; i < playCards * 2; i++) {
-        if (totalRandSuits[index[i]] === "Piki") {
-            cardIcon = "iconPiki"
-        }
-        if (totalRandSuits[index[i]] === "Cherv") {
-            cardIcon = "iconCherv"
-        }
-        if (totalRandSuits[index[i]] === "Bubn") {
-            cardIcon = "iconBubn"
-        }
-        if (totalRandSuits[index[i]] === "Krest") {
-            cardIcon = "iconKrest"
-        }
-        const cardElement = `
+    if (totalRandSuits[index[i]] === "Cherv") {
+      cardIcon = "iconCherv"
+    }
+    if (totalRandSuits[index[i]] === "Bubn") {
+      cardIcon = "iconBubn"
+    }
+    if (totalRandSuits[index[i]] === "Krest") {
+      cardIcon = "iconKrest"
+    }
+    const cardElement = `
                 <div class="card-open" data-id="${index[i]}" data-card="${
-                    totalRandCards[index[i]]
+                  totalRandCards[index[i]]
                 }" data-icon="${cardIcon}" >
                         <div class="cardIn card-heder">${
-                            totalRandCards[index[i]]
+                          totalRandCards[index[i]]
                         }</div>
                         <div class="cardIn card-heder--${cardIcon} "></div>
                         <div class="cardIn card-middle--${cardIcon} "></div>
                         <div class="cardIn card-down--${cardIcon} "></div>
                         <div class="cardIn card-down ">${
-                            totalRandCards[index[i]]
+                          totalRandCards[index[i]]
                         }</div>
                 </div>`
-        cardsField.push(cardElement)
+    cardsField.push(cardElement)
+  }
+  const fieldElement = document.querySelector(".card-forView")
+  for (const i of cardsField) {
+    if (fieldElement) {
+      fieldElement.innerHTML = fieldElement.innerHTML + i
     }
-    const fieldElement = document.querySelector(".card-forView")
-    for (const i of cardsField) {
-        if (fieldElement) {
-            fieldElement.innerHTML = fieldElement.innerHTML + i
-        }
-    }
-    // Вызвать функцию которая закроет карты через 5 секунд
-    setTimeout(closeCardFunction, 3000)
-    return {totalRandCards}
+  }
+  // Вызвать функцию которая закроет карты через 5 секунд
+  setTimeout(closeCardFunction, 3000)
+  return { totalRandCards }
 }
 
 // передача данных для проверок
 
 // module.exports = { mineGameField }
 
-
-
 // Функция закрытия карт
 const closeCardFunction = () => {
-    const cardElements: NodeListOf<Element> =
-        document.querySelectorAll(".card-open")
-    const cardInElements: NodeListOf<Element> =
-        document.querySelectorAll(".cardIn")
-    for (const cardInElement of cardInElements as any) {
-        cardInElement.classList.add("displayNone")
-    }
-    for (const cardElement of cardElements as any) {
-        cardElement.classList.add("card")
-        cardElement.classList.remove("card-open")
-    }
-    gamePlayFunction()
+  const cardElements: NodeListOf<Element> =
+    document.querySelectorAll(".card-open")
+  const cardInElements: NodeListOf<Element> =
+    document.querySelectorAll(".cardIn")
+  for (const cardInElement of cardInElements as any) {
+    cardInElement.classList.add("displayNone")
+  }
+  for (const cardElement of cardElements as any) {
+    cardElement.classList.add("card")
+    cardElement.classList.remove("card-open")
+  }
+  gamePlayFunction()
 }
 const gamePlayFunction = () => {
-    // let openCardCount = 0;
-    let openCardNunber = 0
-    let rangFirstCard = ""
-    let iconFirstCard = ""
-    let result = 0
-    const cardElements: NodeListOf<Element> = document.querySelectorAll(".card")
-    console.log({ cardElements })
-    for (const cardElement of cardElements as any) {
-        cardElement.addEventListener("click", () => {
-            // const cardChildElements = document.querySelectorAll(".cardIn").children
-            const cardChildElements = cardElement.children
-            for (const cardChildElement of cardChildElements) {
-                cardChildElement.classList.remove("displayNone")
-            }
-            cardElement.classList.add("card-open")
-            openCardNunber = openCardNunber + 1
-            if (openCardNunber % 2 !== 0) {
-                rangFirstCard = cardElement.dataset.card
-                iconFirstCard = cardElement.dataset.icon
-            }
-            if (openCardNunber % 2 === 0) {
-                const rangSecondCard = cardElement.dataset.card
-                const iconSecondCard = cardElement.dataset.icon
-                if (
-                    rangSecondCard === rangFirstCard &&
-                    iconSecondCard === iconFirstCard
-                ) {
-                    // добавить функцию выигрыша
-                    result = 1
-                } else {
-                    result = 0
-                }
-                celebrationFunction(result)
-            }
-        })
-    }
-    timerFunction()
+  // let openCardCount = 0;
+  let openCardNunber = 0
+  let rangFirstCard = ""
+  let iconFirstCard = ""
+  let result = 0
+  const cardElements: NodeListOf<Element> = document.querySelectorAll(".card")
+  console.log({ cardElements })
+  for (const cardElement of cardElements as any) {
+    cardElement.addEventListener("click", () => {
+      // const cardChildElements = document.querySelectorAll(".cardIn").children
+      const cardChildElements = cardElement.children
+      for (const cardChildElement of cardChildElements) {
+        cardChildElement.classList.remove("displayNone")
+      }
+      cardElement.classList.add("card-open")
+      openCardNunber = openCardNunber + 1
+      if (openCardNunber % 2 !== 0) {
+        rangFirstCard = cardElement.dataset.card
+        iconFirstCard = cardElement.dataset.icon
+      }
+      if (openCardNunber % 2 === 0) {
+        const rangSecondCard = cardElement.dataset.card
+        const iconSecondCard = cardElement.dataset.icon
+        if (
+          rangSecondCard === rangFirstCard &&
+          iconSecondCard === iconFirstCard
+        ) {
+          // добавить функцию выигрыша
+          result = 1
+        } else {
+          result = 0
+        }
+        celebrationFunction(result)
+      }
+    })
+  }
+  timerFunction()
 }
 const celebrationFunction = (result: number) => {
-    let resultText = ""
-    const hederTimer = document.querySelector(".heder-time")
-    const messageWindow: HTMLElement | null =
-        document.querySelector(".modal-window")
-    clearInterval(2)
-    messageWindow?.classList.add("modal")
-    result === 1
-        ? (resultText = "Вы выиграли!")
-        : (resultText = "Вы проиграли!")
-    messageWindow!.innerHTML = `<div class="finish-game-field">
+  let resultText = ""
+  const hederTimer = document.querySelector(".heder-time")
+  const messageWindow: HTMLElement | null =
+    document.querySelector(".modal-window")
+  clearInterval(2)
+  messageWindow?.classList.add("modal")
+  result === 1 ? (resultText = "Вы выиграли!") : (resultText = "Вы проиграли!")
+  messageWindow!.innerHTML = `<div class="finish-game-field">
     <div class="start-box start">
     <div class= ${
-        result === 1 ? "finish-box--img__celebr" : "finish-box--img__dead"
+      result === 1 ? "finish-box--img__celebr" : "finish-box--img__dead"
     }>   </div>
     <p class="finish-box--text"> ${resultText}</p>
         <div class="finish-box--time">
--- main.ts
+++ main.ts
@@ -4,25 +4,25 @@ let difficultLevel: string = "0"
 
 // Функция выделения кнопки уровня сложности
 const diffLevelButtonColor = () => {
-    // let difficultLevel = 0;
+  // let difficultLevel = 0;
 
-    const difficulteElements: NodeListOf<Element>  = document.querySelectorAll(
-        ".start-box--difficultValue",
-    )
+  const difficulteElements: NodeListOf<Element> = document.querySelectorAll(
+    ".start-box--difficultValue",
+  )
 
-    for (const difficulteElement of difficulteElements as any) {
-        difficulteElement.addEventListener("click", () => {
-            if (difficultLevel !== difficulteElement.name) {
-                for (const difficulteElement of difficulteElements as any) {
-                    difficulteElement.classList.remove("color")
-                }
+  for (const difficulteElement of difficulteElements as any) {
+    difficulteElement.addEventListener("click", () => {
+      if (difficultLevel !== difficulteElement.name) {
+        for (const difficulteElement of difficulteElements as any) {
+          difficulteElement.classList.remove("color")
+        }
 
-                difficulteElement.classList.add("color")
-            }
-            // Записываем уровень сложности
-            return (difficultLevel = difficulteElement.name)
-        })
-    }
+        difficulteElement.classList.add("color")
+      }
+      // Записываем уровень сложности
+      return (difficultLevel = difficulteElement.name)
+    })
+  }
 }
 
 diffLevelButtonColor()
@@ -30,18 +30,19 @@ diffLevelButtonColor()
 // Функция старта игры, выбор сложности
 
 const startGame = () => {
-    const startElement: HTMLElement | null = document.querySelector(".start-box--button")
-    // const fieldElement = document.querySelector(".start-game-field")
-    startElement?.addEventListener("click", () => {
-        if (difficultLevel === "0") {
-            return alert("Выберите сложность игры")
-        } else {
-            // fieldElement.innerHTML = renderHeder()
-            renderHeder()
-            // fieldElement.classList.remove("start-game-field")
-            mineGameField(difficultLevel)
-        }
-    })
+  const startElement: HTMLElement | null =
+    document.querySelector(".start-box--button")
+  // const fieldElement = document.querySelector(".start-game-field")
+  startElement?.addEventListener("click", () => {
+    if (difficultLevel === "0") {
+      return alert("Выберите сложность игры")
+    } else {
+      // fieldElement.innerHTML = renderHeder()
+      renderHeder()
+      // fieldElement.classList.remove("start-game-field")
+      mineGameField(difficultLevel)
+    }
+  })
 }
 
 startGame()
