28b53e7783e827de3660d404ec2fc67b9d772a19
alexkm59 <alexkm59@yandex.ru>
2023-09-24 09:59:03 +0500

    косметика

--- first_game_field.ts
+++ first_game_field.ts
@@ -102,9 +102,6 @@ export const mineGameField = (difficultLevel: string) => {
   return { totalRandCards }
 }
 
-// передача данных для проверок
-
-// module.exports = { mineGameField }
 
 // Функция закрытия карт
 const closeCardFunction = (playCards: number) => {
